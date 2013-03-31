28c33505785415f824977dc4a5db98c43e8f2be4
gmiranda23 <gmiranda@opscode.com>
2013-03-31 16:46:40 -0700

    fixed library, made timeouts in py script uniform

--- libraries/nvp.rb
+++ libraries/nvp.rb
@@ -3,7 +3,7 @@ class Chef::Recipe::NVP
   def self.error
     puts "================================================================================"
     puts "Recipe Requirement Error - Nicira OpenVswitch packages not found"
-    puts "To fix this error, place Nicira OpenVswitch packages in #{node['nvp']['ovs']['pkg_path']}"
+    puts "To fix this error, place Nicira OpenVswitch packages in the expected pkg_path"
     puts "================================================================================"
   end
 
@@ -12,7 +12,7 @@ class Chef::Recipe::NVP
   def self.checkpkg(pkgfile)
     unless !Dir.glob("#{pkgfile}*.deb").empty?
       self.error
-      Chef::Application.fatal!("Nicira package not found at #{pkgfile}")
+      Chef::Application.fatal!("Nicira package not found at #{pkgfile}*.deb")
     end
   end
 
